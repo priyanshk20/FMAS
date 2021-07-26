3420dc8c43007a105eea055f93546b392411d89e
priyanshk20 <khandelwalpriyansh85@gmail.com>
2021-07-26 22:30:21 +0530

    mapping left

--- src/App.js
+++ src/App.js
@@ -7,14 +7,39 @@ const App = () => {
   return (
     <>
      <Navbar/>
-     <Card/>
-     <Card/>
-     <Card/>
-     <Card/>
-     <Card/>
-     <Card/>
-     <Card/>
-     <Card/>
+     <Card imgsrc = "https://i.pinimg.com/736x/a0/70/b6/a070b684e47e4d5c8a94302b269e574a.jpg"
+          sname = "Friends"
+          link = "https://www.netflix.com/in/title/70153404"
+          place = "A NETFLIX Original"
+     />
+
+     <Card sname ="Money Heist"
+        place = "A NETFLIX Original" 
+        imgsrc = "https://wallpapercave.com/wp/wp6201564.jpg"
+        link= "https://www.netflix.com/in/title/80192098"
+     />
+
+     <Card sname = "Andhadhun"
+        place = "A NETFLIX Original"
+        imgsrc = "https://mir-s3-cdn-cf.behance.net/project_modules/fs/96245c69898165.5b96482709d56.jpg"
+        link = "https://www.netflix.com/in/title/81039381"
+     />
+     <Card  sname ="The Family Man"
+        place = "Amazon Prime Original"
+        imgsrc = "https://m.media-amazon.com/images/M/MV5BMzRjZWVmMzItNTdmYS00OWEzLTgyOGUtNThiNTU2ZThlYjY0XkEyXkFqcGdeQXVyMTIxMDk2NDE4._V1_.jpg"
+        link ="https://www.primevideo.com/detail/The-Family-Man/0H3DDB4KBJFNDCKKLHNRLRLVKQ"
+     
+     />
+     <Card sname ="Breaking Bad"
+        place ="A NETFLIX Original"
+        imgsrc = "https://m.media-amazon.com/images/M/MV5BMjhiMzgxZTctNDc1Ni00OTIxLTlhMTYtZTA3ZWFkODRkNmE2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY1200_CR116,0,630,1200_AL_.jpg"
+        link = "https://www.netflix.com/in/title/70143836" 
+     />
+     <Card sname ="The Shawshank Redemption"
+        place ="A NETFLIX Original"
+        imgsrc = "https://vistapointe.net/images/the-shawshank-redemption-wallpaper-18.jpg"
+        link = "https://www.netflix.com/in/title/70005379"  
+     />
     </>
   )
 }
--- src/Card.js
+++ src/Card.js
@@ -1,20 +1,61 @@
 import React from 'react'
 import './styles/card.css'
 
-const Card = () => {
+const movies = [
+    {
+        name:"Money Heist",
+        place:"A NETFLIX Original",
+        img: "https://wallpapercave.com/wp/wp6201564.jpg",
+        link:"https://www.netflix.com/in/title/80192098",
+    },
+    {
+        name:"Friends",
+        place:"A NETFLIX Original",
+        img: "https://i.pinimg.com/736x/a0/70/b6/a070b684e47e4d5c8a94302b269e574a.jpg",
+        link:"https://www.netflix.com/in/title/70153404",
+    },
+    {
+        name:"Andhadhun",
+        place:"A NETFLIX Original",
+        img: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/96245c69898165.5b96482709d56.jpg",
+        link:"https://www.netflix.com/in/title/81039381",
+    },
+    {
+        name:"The Family Man",
+        place:"Amazon Prime Original",
+        img: "https://m.media-amazon.com/images/M/MV5BMzRjZWVmMzItNTdmYS00OWEzLTgyOGUtNThiNTU2ZThlYjY0XkEyXkFqcGdeQXVyMTIxMDk2NDE4._V1_.jpg",
+        link:"https://www.primevideo.com/detail/The-Family-Man/0H3DDB4KBJFNDCKKLHNRLRLVKQ",
+    },
+    {
+        name:"Breaking Bad",
+        place:"A NETFLIX Original",
+        img: "https://m.media-amazon.com/images/M/MV5BMjhiMzgxZTctNDc1Ni00OTIxLTlhMTYtZTA3ZWFkODRkNmE2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY1200_CR116,0,630,1200_AL_.jpg",
+        link:"https://www.netflix.com/in/title/70143836",
+    },
+    {
+        name:"The Shawshank Redemption",
+        place:"A NETFLIX Original",
+        img: "https://vistapointe.net/images/the-shawshank-redemption-wallpaper-18.jpg",
+        link:"https://www.netflix.com/in/title/70005379",
+    }
+
+]
+
+const Card = (props) => {
+    console.log(props);
     return (
         <>
     <div className="card-container">
     <div className="card">
              <div className="card-img">
-                 <img src="https://wallpapercave.com/wp/wp6201564.jpg" alt="money heist" />
+                 <img src={props.imgsrc} alt="money heist" />
              </div>
              <div className="card-info">
-                 <h3>Money Heist</h3>
-                 <span>A NETLIX ORIGINAL SERIES</span>
+                 <h3> {props.sname} </h3>
+                 <span>{props.place} </span>
              </div>
              <div className="watch-btn">
-                <button><a href="https://www.netflix.com/in/title/80192098" target="_blank" rel="noreferrer">Watch Now</a></button>
+                <button><a href={props.link} target="_blank" rel="noreferrer">Watch Now</a></button>
              </div>
          </div>
     </div>

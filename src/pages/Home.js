9a5508cbcc05cc2d725b795221edc147c9b4e7b1
priyanshk20 <khandelwalpriyansh85@gmail.com>
2022-02-04 14:19:13 +0530

    added routing

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../components/Banner';
+import Movies from '../components/Movies';
+import Pagination from '../components/Pagination';
+
+const Home = () => {
+  return <>
+  <Banner/>
+  <Movies/>
+  <Pagination/>
+  </>;
+};
+
+export default Home;

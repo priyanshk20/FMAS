eb11f92e90a96e858031e2fa5a1972984c1e88b8
priyanshk20 <khandelwalpriyansh85@gmail.com>
2022-02-06 13:12:26 +0530

    pagination state

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Pagination = () => {
+    const [page, setPage] = useState(1)
+
+    const goAhead = () => {
+        setPage(page + 1)
+    }
+
+    const goBack = () => {
+        if (page > 1) {
+            setPage(page - 1)
+        }
+    }
+
     return (
         <>
             <div className="w-full flex justify-center mb-8">
-                <button className="p-2 border-2 border-indigo-500 text-indigo-500 border-r-0 rounded-l-xl">
+                <button
+                    onClick={goBack}
+                    className="p-2 border-2 border-indigo-500 text-indigo-500 border-r-0 rounded-l-xl"
+                >
                     Previous
                 </button>
                 <button className="p-2 border-2 border-indigo-500 text-indigo-500 bg-gray-300">
-                    1
+                    {page}
                 </button>
-                <button className="p-2 border-2 border-indigo-500 text-indigo-500 border-l-0 rounded-r-xl">
+                <button
+                    onClick={goAhead}
+                    className="p-2 border-2 border-indigo-500 text-indigo-500 border-l-0 rounded-r-xl"
+                >
                     Next
                 </button>
             </div>

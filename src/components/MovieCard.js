df3754ca3ffb2a20687db8eb64d8378010d7a040
priyanshk20 <khandelwalpriyansh85@gmail.com>
2022-02-07 14:52:37 +0530

    integrating tmdb api using axios

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,15 +1,14 @@
 import React from 'react'
-import BannerImage from '../assests/nowayhome.jpg'
 
-const MovieCard = () => {
+const MovieCard = ({ title, key, image }) => {
     return (
         <>
             <div className="flex px-8 py-8">
                 <div
-                    className={`bg-[url(${BannerImage})]  h-[25vh] w-[125px]  md:h-[30vh] md:w-[250px] bg-center bg-cover rounded-xl flex items-end hover:scale-110 ease-out duration-300`}
+                    className={`bg-[url(https://image.tmdb.org/t/p/w500${image})]  h-[25vh] w-[125px]  md:h-[30vh] md:w-[250px] bg-center bg-cover rounded-xl flex items-end hover:scale-110 ease-out duration-300`}
                 >
                     <div className="text-white p-2 bg-black w-full text-center font-bold rounded-b-xl">
-                        Spider-Man : No Way Home
+                        {title}
                     </div>
                 </div>
             </div>
